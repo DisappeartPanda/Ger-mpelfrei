3caa286abe1ff2d5cd10642e56b78089ac72027e
Panda <fabian.steinberg@gmx.de>
2026-04-07 21:23:17 +0200

    s

---
+++ src/site.config.js
@@ -0,0 +1,7 @@
+export const SITE = {
+  name: "123 Gerümpelfrei",
+  area: "Hannover + 20 km Umkreis (ab Hannover Mitte)",
+  areaShort: "Hannover (20 km ab Mitte)",
+  phone: "+49XXXXXXXXXX",
+  email: "info@geruempelfrei.de",
+};
